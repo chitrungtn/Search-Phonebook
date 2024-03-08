07181474a742cdeda48e5d8cc67801596df7a473
chitrungtn <tuannl98tn@gmail.com>
2024-03-08 23:12:28 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes >
-        <Route path='/' element={<Home />} />
+        <Route path='/Search-Phonebook/' element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
